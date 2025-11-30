6e6143cecc9bc2034b327f35a1c00b447ecd0258
TrietLe347 <le347@csusm.edu>
2025-11-30 15:36:14 -0800

    testing failed build
    Commented out private static Vector2 calculateVelocity(Random random):
    line 124 file Asteroids.java

--- src/main/java/edu/csusm/group12/asteroids/entity/Asteroid.java
+++ src/main/java/edu/csusm/group12/asteroids/entity/Asteroid.java
@@ -119,10 +119,12 @@ public class Asteroid extends Entity {
 	 * @param random The random instance.
 	 * @return The velocity.
 	 */
+	
+	/*
 	private static Vector2 calculateVelocity(Random random) {
 		return new Vector2(random.nextDouble() * Math.PI * 2).scale(MIN_VELOCITY + random.nextDouble() * VELOCITY_VARIANCE);
 	}
-	
+	*/
 	@Override
 	public void update(Game game) {
 		super.update(game);
