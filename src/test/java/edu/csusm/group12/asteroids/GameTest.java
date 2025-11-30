da130b203327e831ee5d000ff55482840b0438fc
TrietLe347 <le347@csusm.edu>
2025-11-30 13:59:59 -0800

    Created Project
    Created the original Maven project with Asteroids files added into the
    project with the original structure of the project

---
+++ src/test/java/edu/csusm/group12/asteroids/GameTest.java
@@ -0,0 +1,13 @@
+package edu.csusm.group12.asteroids;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    @Test
+    void testGameLoads() {
+        // Just a sanity check that JUnit is working
+        assertEquals(2, 1 + 1);
+    }
+}
